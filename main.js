3ab3d6a30b391c16799ee4fd533a2aa4c46966f5
Asfa Qasim <160766420+AsfaQasim@users.noreply.github.com>
2024-03-07 17:16:16 -0800

    Q 05 of Ts/js & node js of 45 assignment
    Q 05 of Ts/js & node js of 45 assignment

---
+++ main.js
@@ -0,0 +1,4 @@
+var quote = "A person who never made a mistake never tried anything new";
+var famous_person = "Albert Einstein";
+var message = "".concat(famous_person, " once said, ").concat(quote);
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,4 @@
+let quote: string= "A person who never made a mistake never tried anything new";
+let famous_person= "Albert Einstein";
+let message= `${famous_person} once said, ${quote}`;
+console.log(message);
